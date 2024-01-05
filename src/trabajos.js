851b3be52fa3486adb0f2e669e45bddbf8880ab5
Jonatan Missora <jonatanjmissora@gmail.com>
2024-01-05 17:12:59 -0300

    05-01

---
+++ src/trabajos.js
@@ -0,0 +1,263 @@
+export const trabajos = {
+  "bahia01": {
+    nombre: "Bahia Confort",
+    categoria: "Apart Hotel",
+    portada: "/assets/gallery/bahia1/bahiaconfort01.webp",
+    galeria: [
+      "/assets/360.svg",
+      "/assets/gallery/bahia1/bahiaconfort01.webp",
+      "/assets/gallery/bahia1/bahiaconfort02.webp",
+      "/assets/gallery/bahia1/bahiaconfort03.webp",
+      "/assets/gallery/bahia1/bahiaconfort04.webp",
+      "/assets/gallery/bahia1/bahiaconfort05.webp",
+      "/assets/gallery/bahia1/bahiaconfort06.webp",
+      "/assets/gallery/bahia1/bahiaconfort07.webp",
+    ],
+    tour: "https://www.keepeyeonball.com/Tour/78240ece-c23f-41b8-87c1-86f565a31cb8"
+  },
+  "orion01": {
+    nombre: "Hotel Orion",
+    categoria: "Apart Hotel",
+    portada: "/assets/trabajos/hotel_orion.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/7c4bc1e6-ae3d-449d-8da8-f86cbae58a42"
+  },
+  "alem01": {
+    nombre: "Punto Alem",
+    categoria: "Salones",
+    portada: "/assets/trabajos/punto_alem.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/45cd220d-41f4-44f8-aa26-c1a485a47f86"
+  },
+  "top501": {
+    nombre: "Top 5",
+    categoria: "Salones",
+    portada: "/assets/trabajos/top5.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/de891ac9-ac4a-4a6a-be79-9b561a6772d6/4K?sc=8&y=180&p=0&tz=100&r=0.00;0.00&l=0.00;0.00&mz=0.00&d=0&m=0",
+  },
+  "top601": {
+    nombre: "Top 6",
+    categoria: "Salones",
+    portada: "/assets/trabajos/top6.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/3ec16edb-1db2-434a-9020-d8e55f04fb1f/4K?sc=8&y=180&p=0&tz=100&r=0.00;0.00&l=0.00;0.00&mz=0.00&d=0&m=0",
+  },
+  "casali00": {
+    nombre: "Casali Prop",
+    categoria: "Propiedades",
+    portada: "/assets/gallery/casali0/casaliprop6.webp",
+    galeria: [
+      "/assets/360.svg",
+      "/assets/gallery/casali0/casaliprop1.webp",
+      "/assets/gallery/casali0/casaliprop2.webp",
+      "/assets/gallery/casali0/casaliprop3.webp",
+      "/assets/gallery/casali0/casaliprop4.webp",
+      "/assets/gallery/casali0/casaliprop5.webp",
+      "/assets/gallery/casali0/casaliprop6.webp",
+      "/assets/gallery/casali0/casaliprop7.webp",
+    ],
+    tour: "https://kuula.co/share/collection/7XJmF?logo=1&info=1&fs=1&vr=0&zoom=1&gyro=0&autorotate=0.3&thumbs=1&alpha=0.60&inst=es",
+  },
+  "casali01": {
+    nombre: "Casali Prop",
+    categoria: "Propiedades",
+    portada: "/assets/gallery/casali1/casaliprop2.webp",
+    galeria: [
+      "/assets/360.svg",
+      "/assets/gallery/casali1/casaliprop1.webp",
+      "/assets/gallery/casali1/casaliprop2.webp",
+      "/assets/gallery/casali1/casaliprop3.webp",
+      "/assets/gallery/casali1/casaliprop4.webp",
+      "/assets/gallery/casali1/casaliprop5.webp",
+      "/assets/gallery/casali1/casaliprop6.webp",
+      "/assets/gallery/casali1/casaliprop7.webp",
+    ],
+    tour: "https://www.keepeyeonball.com/Tour/d5dc8dcf-cf4d-4bbf-99d1-c4f72b2dd875",
+  },
+  "casali02": {
+    nombre: "Casali Prop",
+    categoria: "Propiedades",
+    portada: "/assets/gallery/casali2/casaliprop1.webp",
+    galeria: [
+      "/assets/360.svg",
+      "/assets/gallery/casali2/casaliprop1.webp",
+      "/assets/gallery/casali2/casaliprop2.webp",
+      "/assets/gallery/casali2/casaliprop3.webp",
+      "/assets/gallery/casali2/casaliprop4.webp",
+      "/assets/gallery/casali2/casaliprop5.webp",
+      "/assets/gallery/casali2/casaliprop6.webp",
+      "/assets/gallery/casali2/casaliprop7.webp",
+    ],
+    tour: "https://www.keepeyeonball.com/Tour/d850794d-c703-427e-9707-5ce711bdf587",
+  },
+  "casali03": {
+    nombre: "Casali Prop",
+    categoria: "Propiedades",
+    portada: "/assets/gallery/casali3/casaliprop05.webp",
+    galeria: [
+      "/assets/360.svg",
+      "/assets/gallery/casali3/casaliprop01.webp",
+      "/assets/gallery/casali3/casaliprop02.webp",
+      "/assets/gallery/casali3/casaliprop03.webp",
+      "/assets/gallery/casali3/casaliprop04.webp",
+      "/assets/gallery/casali3/casaliprop05.webp",
+      "/assets/gallery/casali3/casaliprop06.webp",
+      "/assets/gallery/casali3/casaliprop07.webp",
+      "/assets/gallery/casali3/casaliprop08.webp",
+      "/assets/gallery/casali3/casaliprop09.webp",
+      "/assets/gallery/casali3/casaliprop10.webp",
+      "/assets/gallery/casali3/casaliprop11.webp",
+    ],
+    tour: "https://www.keepeyeonball.com/Tour/559ef560-2c52-4193-ac0b-d8ee46249b62",
+  },
+  "casali04": {
+    nombre: "Casali Prop",
+    categoria: "Propiedades",
+    portada: "/assets/gallery/casali4/casaliprop04.webp",
+    galeria: [
+      "/assets/360.svg",
+      "/assets/gallery/casali4/casaliprop01.webp",
+      "/assets/gallery/casali4/casaliprop02.webp",
+      "/assets/gallery/casali4/casaliprop03.webp",
+      "/assets/gallery/casali4/casaliprop04.webp",
+      "/assets/gallery/casali4/casaliprop05.webp",
+      "/assets/gallery/casali4/casaliprop06.webp",
+      "/assets/gallery/casali4/casaliprop07.webp",
+      "/assets/gallery/casali4/casaliprop08.webp",
+      "/assets/gallery/casali4/casaliprop09.webp",
+      "/assets/gallery/casali4/casaliprop10.webp",
+      "/assets/gallery/casali4/casaliprop11.webp",
+      "/assets/gallery/casali4/casaliprop12.webp",
+      "/assets/gallery/casali4/casaliprop13.webp",
+      "/assets/gallery/casali4/casaliprop14.webp",
+      "/assets/gallery/casali4/casaliprop15.webp",
+    ],
+    tour: "https://www.keepeyeonball.com/Tour/5a9f008e-d8c7-42af-812a-36ef16abc16d",
+  },
+  "obra01": {
+    nombre: "Avance de obra",
+    categoria: "Propiedades",
+    portada: "/assets/gallery/obra/obra1.webp",
+    galeria: [
+      "/assets/360.svg",
+      "/assets/gallery/obra/obra1.webp",
+      "/assets/gallery/obra/obra2.webp",
+      "/assets/gallery/obra/obra3.webp",
+      "/assets/gallery/obra/obra4.webp",
+      "/assets/gallery/obra/obra5.webp",
+      "/assets/gallery/obra/obra6.webp",
+      "/assets/gallery/obra/obra7.webp",
+    ],
+    tour: "https://www.keepeyeonball.com/Tour/deaabb51-4c18-4a43-acf3-3c4e52a06201",
+  },
+
+  "iagro01": {
+    nombre: "I.A.G.R.O Inmobiliaria",
+    categoria: "Comercios",
+    portada: "/assets/gallery/IAGRO/iagro1.webp",
+    galeria: [
+      "/assets/gallery/IAGRO/iagro1.webp",
+      "/assets/gallery/IAGRO/iagro2.webp",
+      "/assets/gallery/IAGRO/iagro3.webp",
+    ],
+    tour: "",
+  },
+  "mini01": {
+    nombre: "Minimalismo",
+    categoria: "Propia",
+    portada: "/assets/gallery/Minimalismo/minimal1.webp",
+    galeria: [
+      "/assets/gallery/Minimalismo/minimal1.webp",
+      "/assets/gallery/Minimalismo/minimal2.webp",
+      "/assets/gallery/Minimalismo/minimal3.webp",
+      "/assets/gallery/Minimalismo/minimal4.webp",
+      "/assets/gallery/Minimalismo/minimal5.webp",
+      "/assets/gallery/Minimalismo/minimal6.webp",
+      "/assets/gallery/Minimalismo/minimal7.webp",
+      "/assets/gallery/Minimalismo/minimal8.webp",
+    ],
+    tour: "",
+  },
+
+  "moto01": {
+    nombre: "Moto Manía",
+    categoria: "Comercios",
+    portada: "/assets/trabajos/moto_mania.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/f021accf-ad02-4b26-b2e1-2213c2dc6ffd",
+  },
+  "dexter01": {
+    nombre: "Dexter",
+    categoria: "Comercios",
+    portada: "/assets/trabajos/dexter.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/f0ebfa0d-a7ee-42ca-a954-c6bd24ef254a",
+  },
+  "varoba01": {
+    nombre: "Varoba",
+    categoria: "Comercios",
+    portada: "/assets/trabajos/varoba.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/fd5fa629-5246-4aa5-9bbc-c8e98e39285b",
+  },
+  "ragazzi01": {
+    nombre: "Ragazzi",
+    categoria: "Comercios",
+    portada: "/assets/trabajos/ragazzi.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/b7eff8f5-2b04-400a-830e-cbb99f3f7e3a",
+  },
+  "tekno01": {
+    nombre: "Teknomanía",
+    categoria: "Comercios",
+    portada: "/assets/trabajos/teknomania.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/9fe2704e-d383-46df-a8e1-8c9c613e5288",
+  },
+  "alba01": {
+    nombre: "Alba Garcia",
+    categoria: "Comercios",
+    portada: "/assets/trabajos/alba_garcia.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/5eee8952-4066-4f00-9336-8b9c4c47005c",
+  },
+  "atila01": {
+    nombre: "Atila",
+    categoria: "Comercios",
+    portada: "/assets/trabajos/atila.svg",
+    galeria: [],
+    tour: "https://www.keepeyeonball.com/Tour/e80a543d-ff46-46d3-b52a-2d0cd4aee1aa",
+  },
+}
+
+export const tourCategorias = [
+  {
+    nombre: "Apart Hotel",
+    tours: ["bahia01", "orion01"]
+  },
+  {
+    nombre: "Propiedades",
+    tours: ["casali00", "casali01", "casali02", "casali03", "casali04", "obra01"]
+  },
+  {
+    nombre: "Salones",
+    tours: ["alem01", "top501", "top601"]
+  },
+  {
+    nombre: "Comercios",
+    tours: ["moto01", "dexter01", "varoba01", "ragazzi01", "tekno01", "alba01", "atila01"]
+  }
+]
+
+export const galeriaColecciones = [
+  "bahia01",
+  "casali00",
+  "casali01",
+  "casali02",
+  "casali03",
+  "casali04",
+  "obra01",
+  "iagro01",
+  "mini01",
+];
